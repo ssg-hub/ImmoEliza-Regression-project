11b08db556ff9b51ade90d720a972489c77b45c2
harozudu <harozudu@gmail.com>
2021-07-16 12:54:41 +0200

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 12:28:15 2021
 
+@author: HZU
+"""
 
 import streamlit as st
 import pandas as pd
@@ -20,7 +24,7 @@ def cleaning_data(df):
     #Ahora son 10795
     df = df.dropna(subset=['area'])
     #Ahora son 9543
-    df = df.dropna(subset=['building_condition'])
+    # df = df.dropna(subset=['building_condition'])
     df = df.dropna(subset=['location_lat']) 
     df = df.dropna(subset=['location_lon'])     
     #Ahora son 7059
@@ -56,20 +60,7 @@ def classification_by_region(df):
 
 df = cleaning_data(df)
 
-# df_houses, df_office, df_industry, df_apartment = classification_by_type(df)
-
-##############################################################################
-
 st.set_page_config(layout="wide")
-
-st.title('Real estate analysis in Belgium using sklearn library')
-st.write('\n')
-st.subheader('This application analyzes the real state prices in Belgium for apartments, houses and industry.')
-st.subheader('Also the search is divided by regions:')
-st.subheader('Flanders, Wallonia, Brussels.')
-st.write('The data used in this demo was scrape from Immoweb on 06/28/2021. A total of 12,128 locations will be analyzed.')
-st.write('\n')
-st.write('\n')
 col1, col2 = st.beta_columns(2)
 
 
@@ -144,20 +135,22 @@ with col1:
             my_bar.progress(percent_complete + 1)   
             
     st.dataframe(df_data)
-    
+
 with col2:
     st.map(df_data)       
 
-    
-st.markdown("""---""")
 st.markdown("""---""")
 st.write('\n')
 
 col1, col2 = st.beta_columns(2)
 
 with col1:
-    st.subheader('To perform a more detailed data analysis, we will use the relationship between the area [m^2] and the price [euro] of each property.')
+    st.subheader('We create 2 arrays')
     st.write('\n')
+    st.write('X = df_data['+'area'+'].to_numpy()')
+    st.write('y = df_data['+'actual_price'+'].to_numpy()')
+    st.write('\n')
+
     X = df_data['area'].to_numpy()
     y = df_data['actual_price'].to_numpy()
     
@@ -172,7 +165,6 @@ with col1:
     plt.ylabel('price')
     plt.xlabel('area')    
     st.write(fig1)
-
 
 with col2:
     st.subheader('We separate the data into two parts, one to create the model and the other to test the model.')
@@ -192,86 +184,41 @@ with col2:
 st.markdown("""---""")
 st.markdown("""---""")
 st.write('\n')
-    
-col1, col2 = st.beta_columns(2)
 
 
-with col1:
-    st.subheader('Now, we create a model with KNeighborsRegressor and analyze the behavior with 5 next neighbors..')
-    st.write('pipe = Pipeline([("model", KNeighborsRegressor(n_neighbors=5))])')
-    st.write('\n')    
-    pipe = Pipeline([
-        ("model", KNeighborsRegressor(n_neighbors=5),
-          )
-    ])
-    pred = pipe.fit(X_train, y_train).predict(X_test)
-
-    fig3 = plt.figure()      
-    # ax3 = plt.figure(figsize=(10, 6))
-    ax3 = plt.scatter(X_test, pred, label='prediction')
-    ax3 = plt.scatter(X_test, y_test, label='test')
-    ax3 = plt.ylabel('price')
-    ax3 = plt.xlabel('area')
-    ax3 = plt.legend()
-    st.write(fig3)
-    
-with col2:
-    st.subheader('But I would like to know the behavior if I chosse 10 neighbors')
-    st.write('mod = GridSearchCV(estimator=pipe, param_grid={'+'model__n_neighbors'+': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}, cv=5)')
-    st.write('\n')
-    
-    mod = GridSearchCV(estimator=pipe,
-                    param_grid={
-                      'model__n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-                    },
-                    cv=5)                   
-                   
-    mod.fit(X_test, y_test);
-    data = pd.DataFrame(mod.cv_results_)
-    st.dataframe(data)
-    
-    st.write('\n')
-    st.write('Considering the plot below we chosse the better model')
-    st.write('\n')   
-
-    fig4 = plt.figure()      
-    ax4 = plt.plot(data.param_model__n_neighbors, data.split0_test_score, label=0)
-    ax4 = plt.plot(data.param_model__n_neighbors, data.split1_test_score, label=1)
-    ax4 = plt.plot(data.param_model__n_neighbors, data.split2_test_score, label=2)
-    ax4 = plt.plot(data.param_model__n_neighbors, data.split3_test_score, label=3)
-    ax4 = plt.plot(data.param_model__n_neighbors, data.split4_test_score, label=4)
-    ax4 = plt.legend()
-    st.write(fig4)
-    
-st.markdown("""---""")
-st.markdown("""---""")
-st.write('\n')
 
 
-col1, col2, col3 = st.beta_columns(3)
-with col2:
-    st.subheader('We now have a model to approximate the value of a property in the location selected')
-    st.write('\n')
-    st.write('Please chosse an area in [m^2] using the slider below to find the approximate value of the property')
-    st.write('\n')
-    a = float(min(X))
-    b = float(max(X))
-    
-    value_selected = st.slider('Area:', a, b)
-    
-    st.write('\n')  
-    
-    z = [float(value_selected)]
-    z = np.array(z).reshape(1, -1)
-    st.write('The approximate price of the property with that area is :', pipe.fit(X_train, y_train).predict(z))
 
-    
-    pipe = Pipeline([("model", KNeighborsRegressor(n_neighbors=10))])
-    pred = pipe.fit(X_train, y_train).predict(X_test)
-    fig5 = plt.figure() 
-    ax5 = plt.scatter(X_test, pred, label='prediction', color='r')
-    ax5 = plt.scatter(X_test, y_test, label='test')
-    ax5 = plt.ylabel('price')
-    ax5 = plt.xlabel('area')
-    ax5 = plt.legend()
-    st.write(fig5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
